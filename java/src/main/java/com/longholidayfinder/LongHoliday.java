4c78c7004b20391a7e678c0b9cf37fcfd9e95ded
milzar <realmilanxavier@gmail.com>
2019-09-13 14:32:42 +0530

    Add. Holiday name to long holiday

--- java/src/main/java/com/longholidayfinder/LongHoliday.java
+++ java/src/main/java/com/longholidayfinder/LongHoliday.java
@@ -2,27 +2,40 @@ package com.longholidayfinder;
 
 import com.google.api.client.util.DateTime;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LongHoliday {
     private DateTime startDate;
     private DateTime endDate;
+    private List<String> holidays;
 
     public LongHoliday(DateTime startDate, DateTime endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
+        holidays = new ArrayList<>();
     }
 
     public int duration() {
         return (int) ((this.endDate.getValue() - this.startDate.getValue()) / (1000 * 60 * 60 * 24)) + 1;
     }
 
-    @Override
-    public String toString() {
-
-        return String.format("%s\t-\t%s\tfor %d days", trimTime(startDate), trimTime(endDate), this.duration());
+    void addHoliday(String holiday) {
+        this.holidays.add(holiday);
     }
 
-    private String trimTime(DateTime some){
-        int endIndex = !some.toString().contains("T") ? some.toString().length() :some.toString().indexOf("T");
-        return some.toString().substring(0,endIndex);
+    @Override
+    public String toString() {
+        StringBuilder holidayList = new StringBuilder();
+        for (String holiday : this.holidays) {
+            holidayList.append(String.format("%s ", holiday));
+        }
+
+        return String.format("%-25s\t%-15s\t%-15s\tfor %d days", holidayList.toString(), trimTime(startDate), trimTime(endDate), this.duration());
+    }
+
+    private String trimTime(DateTime some) {
+        int endIndex = !some.toString().contains("T") ? some.toString().length() : some.toString().indexOf("T");
+        return some.toString().substring(0, endIndex);
     }
 }
