d7760e9646b8aa1f11ff7b478131fc76254f5ace
milzar <realmilanxavier@gmail.com>
2019-09-18 14:11:51 +0530

    Refactor. Restructured tests

--- java/src/test/java/com/longholidayfinder/DateCalculationTest.java
+++ java/src/test/java/com/longholidayfinder/DateCalculationTest.java
@@ -1,24 +1,38 @@
 package com.longholidayfinder;
 
 import com.google.api.client.util.DateTime;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static com.longholidayfinder.DateCalculation.daysAfterDate;
 import static com.longholidayfinder.DateCalculation.yearsFrom;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class DateCalculationTest {
-    @Test
-    void expectEqualWhenComparingCurrentDateWithDate0YearsFromNow() {
-        DateTime now = new DateTime(System.currentTimeMillis());
+    @Nested
+    class YearCalculationTest{
 
-        assertEquals(now, yearsFrom(0,now));
-    }
+        @Test
+        void expectEqualWhenComparingCurrentDateWithDate0YearsFromNow() {
+            DateTime now = new DateTime(System.currentTimeMillis());
 
-    @Test
-    void expectNotEqualWhenComparingCurrentDateWithDate1YearsFromNow() {
-        DateTime now = new DateTime(System.currentTimeMillis());
+            assertEquals(now, yearsFrom(0, now));
+        }
 
-        assertNotEquals(now, yearsFrom(1,now));
+        @Test
+        void expectNotEqualWhenComparingCurrentDateWithDate1YearsFromNow() {
+            DateTime now = new DateTime(System.currentTimeMillis());
+
+            assertNotEquals(now, yearsFrom(1, now));
+        }
+
+        @Test
+        void expectEqualWhenComparingSameDate() {
+            DateTime now = new DateTime("1984-04-12T05:30:00.000+05:30");
+            DateTime yearFromNow = new DateTime("1985-04-12T05:30:00.000+05:30");
+
+            assertEquals(yearFromNow, yearsFrom(1, now));
+        }
     }
 }
