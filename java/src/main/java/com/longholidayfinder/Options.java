3cebb502af9ef89b7ffdbc784218e7578c0f295d
milzar <realmilanxavier@gmail.com>
2019-09-19 12:32:36 +0530

    Add. Options

---
+++ java/src/main/java/com/longholidayfinder/Options.java
@@ -0,0 +1,12 @@
+package com.longholidayfinder;
+
+public enum Options {
+    ;
+    public static final int NOTIFICATION_PERIOD = 115;
+
+    public static final int DAYS_IN_A_YEAR = 365;
+    public static final int SECONDS_TO_MILLISECONDS = 1000;
+    public static final int SECONDS_IN_A_MINUTE = 60;
+    public static final int MINUTES_IN_AN_HOUR = 60;
+    public static final int HOURS_IN_A_DAY = 24;
+}
